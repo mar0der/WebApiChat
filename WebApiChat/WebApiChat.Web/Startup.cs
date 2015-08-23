93b8d3d32c9f9e32cab1c8f3e7383c74382474bb
Petar Petkov <petar.b.petkov@gmail.com>
2015-08-24 01:38:16 +0300

    Dependency container added. (some config problems have to be fixed)

--- WebApiChat/WebApiChat.Web/Startup.cs
+++ WebApiChat/WebApiChat.Web/Startup.cs
@@ -12,7 +12,18 @@ namespace WebApiChat.Web
 {
     #region
 
+    using System.Reflection;
+    using System.Web.Http;
+
+    using Ninject;
+    using Ninject.Web.Common.OwinHost;
+    using Ninject.Web.WebApi.OwinHost;
+
     using Owin;
+
+    using WebApiChat.Data;
+    using WebApiChat.Data.Interfaces;
+    using WebApiChat.Data.Repositories;
 
     #endregion
 
@@ -21,6 +32,23 @@ namespace WebApiChat.Web
         public void Configuration(IAppBuilder app)
         {
             this.ConfigureAuth(app);
+            app.UseNinjectMiddleware(CreateKernel).UseNinjectWebApi(GlobalConfiguration.Configuration);
+        }
+
+        private static StandardKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
+
+            RegisterMappings(kernel);
+            return kernel;
+        }
+
+        public static void RegisterMappings(StandardKernel kernel)
+        {
+            kernel.Bind<IChatData>()
+                .To<WebApiChatData>()
+                .WithConstructorArgument("context", c => new WebApiChatDbContext());
         }
     }
 }
