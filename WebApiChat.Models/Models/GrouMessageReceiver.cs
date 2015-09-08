cbc97af88e123c67bf361fa5a67f02ff9cd642f0
Petar Petkov <petar.b.petkov@gmail.com>
2015-09-09 02:31:09 +0300

    group messageReceiver class

---
+++ WebApiChat.Models/Models/GrouMessageReceiver.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApiChat.Models.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using WebApiChat.Models.Enums;
+
+    public class GroupMessageReceiver
+    {
+        [Key, Column(Order = 0)]
+        public int GroupMessageId { get; set; }
+
+        public virtual GroupMessage GroupMessage { get; set; }
+
+        [Key, Column(Order = 1)]
+        public string ReceiverId { get; set; }
+
+        public virtual User Receiver { get; set; }
+
+        public MessageStatus Status { get; set; }
+    }
+}
